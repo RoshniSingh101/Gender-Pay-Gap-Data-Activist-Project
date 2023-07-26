94b2452ede99fbb67e7d4c65f38d28105033a008
RoshniSingh101 <108078575+RoshniSingh101@users.noreply.github.com>
2023-07-26 18:27:28 -0400

    main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 #import numpy 
 import numpy as np 
-# calculate a 5-number summary
-#from numpy import percentile
 
 
 # The GWC_utilities has functions to help format data printed to the console
@@ -20,7 +18,8 @@ lwd = pd.read_csv("glassdoor.csv")
 
 #-----boxplot for education overall------
 
-print("Today, most people understand that there is a trend in which higher education leads to a higher base pay. \nHowever, can gender be a factor in how much employers pay their employees regardless of their level of education?\n")
+print("Today, most people understand that there is a trend in which higher education leads to a higher base pay.")
+print("However, can gender be a factor in how much employers pay their employees regardless of their level of education?\n")
 
 input("Press enter to see how base pay differs between different levels of education.\n")
 
@@ -29,13 +28,18 @@ lwd.head()
 #print the boxplot
 sns.boxplot(data=lwd, x="Education", y="BasePay", showmeans=True, order=["High School", "College", "Masters","PhD"]).set(title="BasePay vs. Education")
 
-print("The overall trend in these boxplots show us that as the degree of education people recieve increases, their base pay will increase.\nClick on the 'X' to see how initial salary differs between gender of those who have a PhD.\n")
+print("The overall trend in these boxplots show us that as the degree of education people recieve increases, their base pay will increase.")
+print("Click on the 'X' to see how initial salary differs between gender of those who have a PhD.\n")
 
 plt.show()
 
 #-------PhD Data Analysis-------------
 
-print("PhD Five Number Summaries:\nFemales PhD -- Min: $36,548, Q1: $80,975, Median: $97,742.50, Q3: $113,224, Max: $160,614\nMales PhD -- Min: $40,187, Q1: $84,734.50, Median: $101,761.50, Q3: $121,919.75, Max: $179,726\nFrom samples of 106 women and 132 men, on average, there was a $6,199.98 gap in salary between men and women with a PhD according to Glassdoor in 2019. Furthermore, statistical tests such as a two-sample t-interval reveal to us that we can be 90% confident that the true gender pay gap between men and women with a PhD in 2019 falls between $747.10 and $11,631.\nClick on the 'X' to see how base pay salary differs between men and women with a masters degree.")
+print("PhD Five Number Summaries:")
+print("Females PhD -- Min: $36,548, Q1: $80,975, Median: $97,742.50, Q3: $113,224, Max: $160,614")
+print("Males PhD -- Min: $40,187, Q1: $84,734.50, Median: $101,761.50, Q3: $121,919.75, Max: $179,726")
+print("From samples of 106 women and 132 men, on average, there was a $6,199.98 gap in salary between men and women with a PhD according to Glassdoor in 2019. Furthermore, statistical tests such as a two-sample t-interval reveal to us that we can be 90% confident that the true gender pay gap between men and women with a PhD in 2019 falls between $747.10 and $11,631.")
+print("Click on the 'X' to see how base pay salary differs between men and women with a masters degree.")
 
 #------------boxplot for PhD-------
 phdBooleanList = lwd["Education"] == "PhD"
@@ -49,7 +53,11 @@ plt.show()
 
 #-------Masters Data Analysis-----------
 
-print("\nMasters Five Number Summaries:\nFemales Masters -- Min: $37,780, Q1: $76,194.50, Median: $91,711, Q3: $106,866.50, Max: $155,203\nMales Masters -- Min: $45,915, Q1: $84,878, Median: $100,305, Q3: $116,751, Max: $165,229\nFrom samples of 106 women and 150 men, on average, there was a $8,689.81 gap in salary between men and women with a masters degree according to Glassdoor in 2019. Statistical tests such as a two-sample t-interval reveal to us that we can be 90% confident that the true gender pay gap between men and women with a masters degree in 2019 falls between $3,585 and $13,795.\nClick on the 'X' to see how initial salary differs between gender of those who have a college undergraduate degree.\n")
+print("\nMasters Five Number Summaries:")
+print("Females Masters -- Min: $37,780, Q1: $76,194.50, Median: $91,711, Q3: $106,866.50, Max: $155,203")
+print("Males Masters -- Min: $45,915, Q1: $84,878, Median: $100,305, Q3: $116,751, Max: $165,229")
+print("From samples of 106 women and 150 men, on average, there was a $8,689.81 gap in salary between men and women with a masters degree according to Glassdoor in 2019. Statistical tests such as a two-sample t-interval reveal to us that we can be 90% confident that the true gender pay gap between men and women with a masters degree in 2019 falls between $3,585 and $13,795.")
+print("Click on the 'X' to see how initial salary differs between gender of those who have a college undergraduate degree.\n")
 
 #--------boxplot for masters-------
 mastersBooleanList = lwd["Education"] == "Masters"
@@ -62,7 +70,11 @@ plt.show()
 
 #-------College Data Analysis-----------
 
-print("\nCollege Five Number Summaries:\nFemales College -- Min: $38,613, Q1: $70,975.50, Median: $86,241, Q3: $104,397, Max: $127,608\nMales College -- Min: $36,642, Q1: $77,939, Median: $98,737, Q3: $119,686, Max: $160,460\nFrom samples of 123 women and 118 men, on average, there was a $11,927.37 gap in salary between men and women with an undergraduate degree according to Glassdoor in 2019. Statistical tests such as a two-sample t-interval reveal to us that we can be 90% confident that the true gender pay gap between men and women with an undergraduate degree in 2019 falls between $6,755.40 and $17,101.\nClick on the 'X' to see how initial salary differs between gender of those who have a high school degree.\n")
+print("\nCollege Five Number Summaries:")
+print("Females College -- Min: $38,613, Q1: $70,975.50, Median: $86,241, Q3: $104,397, Max: $127,608")
+print("Males College -- Min: $36,642, Q1: $77,939, Median: $98,737, Q3: $119,686, Max: $160,460")
+print("From samples of 123 women and 118 men, on average, there was a $11,927.37 gap in salary between men and women with an undergraduate degree according to Glassdoor in 2019. Statistical tests such as a two-sample t-interval reveal to us that we can be 90% confident that the true gender pay gap between men and women with an undergraduate degree in 2019 falls between $6,755.40 and $17,101.")
+print("Click on the 'X' to see how initial salary differs between gender of those who have a high school degree.\n")
 
 #--------boxplot for college-------
 collegeBooleanList = lwd["Education"] == "College"
@@ -75,7 +87,11 @@ plt.show()
 
 #-------High School Data Analysis-------
 
-print("\nFive Number Summaries:\nFemales High School -- Min: $34,208, Q1: $65,485, Median: $86,844, Q3: $103,513.75, Max: $149,893\nMales High School -- Min: $47,036, Q1: $74,829, Median: $89,559, Q3: $106,798, Max: $163,208\nFrom samples of 132 women and 133 men, on average, there was a $5,390.15 gap in salary between men and women with a high school degree according to Glassdoor in 2019. Statistical tests such as a two-sample t-interval reveal to us that we can be 90% confident that the true gender pay gap between men and women with a high school degree in 2019 falls between $398.63 and $10,382.\nClick on the 'X' to read the conclusion of my data analysis.\n")
+print("\nHigh School Five Number Summaries:")
+print("Females High School -- Min: $34,208, Q1: $65,485, Median: $86,844, Q3: $103,513.75, Max: $149,893")
+print("Males High School -- Min: $47,036, Q1: $74,829, Median: $89,559, Q3: $106,798, Max: $163,208")
+print("From samples of 132 women and 133 men, on average, there was a $5,390.15 gap in salary between men and women with a high school degree according to Glassdoor in 2019. Statistical tests such as a two-sample t-interval reveal to us that we can be 90% confident that the true gender pay gap between men and women with a high school degree in 2019 falls between $398.63 and $10,382.")
+print("Click on the 'X' to read the conclusion of my data analysis.\n")
 
 #--------boxplot for high school-------
 hsBooleanList = lwd["Education"] == "High School"
